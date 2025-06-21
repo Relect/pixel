fec110ba185751bbf527a48b722bcf1a5971c203
Marchenko <relect@bk.ru>
2025-06-21 12:24:09 +0300

    fix: check

--- src/main/resources/db/migration/001-create-table-user.sql
+++ src/main/resources/db/migration/001-create-table-user.sql
@@ -6,9 +6,4 @@ CREATE TABLE IF NOT EXISTS "user" (
     id bigserial PRIMARY KEY,
     name varchar(500) NOT NULL,
     date_of_birth date NOT NULL,
-    password varchar(500) NOT NULL);
-
-
-
-
-
+    password varchar(500) NOT NULL CHECK (length(password) >= 8));
