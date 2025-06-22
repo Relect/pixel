d912f53731a6b9311ff00dc145e1f41830884145
Marchenko <relect@bk.ru>
2025-06-22 19:18:28 +0300

    feat: add redis

--- src/main/java/com/pixel/demo/api/AuthController.java
+++ src/main/java/com/pixel/demo/api/AuthController.java
@@ -2,10 +2,12 @@ package com.pixel.demo.api;
 
 import com.pixel.demo.dto.ReqRes;
 import com.pixel.demo.service.AuthService;
+import com.pixel.demo.service.RedisBlacklistService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,10 +15,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/auth")
 public class AuthController {
+
     private final AuthService authService;
+    private final RedisBlacklistService blacklistService;
 
     @PostMapping("/signin")
     public ResponseEntity<ReqRes> signIn(@RequestBody ReqRes signInRequest){
         return ResponseEntity.ok(authService.signIn(signInRequest));
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout(
+            @RequestHeader("Authorization") String authHeader) {
+
+        String token = authHeader.substring(7);
+        //long expirationTime = jwtTokenProvider.getRemainingExpirationTime(token); // Оставшееся время жизни
+
+        //todo blacklistService.addToBlacklist(token, expirationTime);
+
+        return ResponseEntity.ok("Logged out successfully");
     }
 }
--- src/main/java/com/pixel/demo/config/Jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/pixel/demo/config/Jwt/JwtAuthenticationFilter.java
@@ -1,19 +1,18 @@
 package com.pixel.demo.config.Jwt;
 
 import com.pixel.demo.model.User;
-import com.pixel.demo.repository.UserRepository;
 import com.pixel.demo.security.JWTUtils;
 import com.pixel.demo.service.CustomUserDetailsService;
+import com.pixel.demo.service.RedisBlacklistService;
+import com.pixel.demo.service.UserService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -26,7 +25,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JWTUtils jwtUtils;
     private final CustomUserDetailsService customUserDetailsService;
-    private final UserRepository userRepository;
+    private final UserService userService;
+    private final RedisBlacklistService blacklistService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -38,10 +38,14 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             return;
         }
         jwtToken = authHeader.substring(7);
+        if (blacklistService.isBlacklisted(jwtToken)) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Token in blacklist");
+            return;
+        }
         userIdStr = jwtUtils.extractUserId(jwtToken);
         if (userIdStr != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             Long userId = Long.parseLong(userIdStr);
-            Optional<User> userOpt = userRepository.findById(userId);
+            Optional<User> userOpt = userService.findById(userId);
             if (userOpt.isPresent()) {
                 User user = userOpt.get();
                 UserDetails userDetails = customUserDetailsService.loadUserByUsername(user.getEmailData().get(0).getEmail());
