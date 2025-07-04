0c5a525a86f8648e5cf94e68b739a4a4220dfcec
Marchenko <relect@bk.ru>
2025-07-04 13:36:58 +0300

    refactor

--- src/main/resources/db/migration/006-create-user1.sql
+++ src/main/resources/db/migration/006-create-user1.sql
@@ -5,8 +5,8 @@
 INSERT INTO "user" (name, date_of_birth, password)
 VALUES ('Ivan', '1990-01-31', '$2a$10$.1AoLkbN9iIAOdH6GECwyeTlzVJLhQMxzCSGHANC/nNummwTKnrYa');
 
-INSERT INTO account (user_id, balance)
-VALUES (1, 100.00);
+INSERT INTO account (user_id, balance, start_balance)
+VALUES (1, 100.00, 100.00);
 
 INSERT INTO email_data (user_id, email)
 VALUES (1, 'email1@example.com');
--- src/main/resources/db/migration/007-create-user2.sql
+++ src/main/resources/db/migration/007-create-user2.sql
@@ -5,8 +5,8 @@
 INSERT INTO "user" (name, date_of_birth, password)
 VALUES ('Misha', '1990-01-31', '$2a$10$.1AoLkbN9iIAOdH6GECwyeTlzVJLhQMxzCSGHANC/nNummwTKnrYa');
 
-INSERT INTO account (user_id, balance)
-VALUES (2, 140.00);
+INSERT INTO account (user_id, balance, start_balance)
+VALUES (2, 140.00, 140.00);
 
 INSERT INTO email_data (user_id, email)
 VALUES (2, 'email1@example2.com');
